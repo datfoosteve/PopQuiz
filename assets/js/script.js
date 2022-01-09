ea545b869263e1d4f8dac715561075f96cc5065a
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-01-08 20:48:24 -0600

    dunno why this is taking forever

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,125 @@
-    // code the click event from the start button to start the timer
-        
+// code the click event from the start button to start the timer
 
-    // code timer
-    // code question - Array of objects;
+// code timer
+// code question - Array of objects;
 
-    // Answer form question by clicking using the click event handler
-    //
-    // Check if question boolean is true, move on to next qurestion on click event(choices)
-                                //is false, subtract from timer 10 seconds and then move on to question from click event.(choices)
+// Answer form question by clicking using the click event handler
+//
+// Check if question boolean is true, move on to next qurestion on click event(choices)
+//is false, subtract from timer 10 seconds and then move on to question from click event.(choices)
 
-    // Post some sort of message stating the game is over page
+// Post some sort of message stating the game is over page
 
-    //Final conditions is when the timer reaches 0 or all the questions are answers
-    // Then - use local storage to store initials (string) of the player.
+//Final conditions is when the timer reaches 0 or all the questions are answers
+// Then - use local storage to store initials (string) of the player.
 var mainEl = document.getElementById("#main");
-var timerEl = document.getElementById("#countDown");
+let timerEl = document.querySelector("#countDown");
+
+const questionEl = document.getElementById("theQuestion");
+let shuffleQuestions, currentQuestion;
+
+let abra = document.querySelector("#gameTimerWrapper");
+let kadabra = document.querySelector("#qContainer");
 var gameTimer = 30;
 var timeInterval;
 
+const startButtonEl = document.querySelector("#Start");
+const aButtonEl = document.querySelector("#Start");
 
-  const startButtonEl = document.querySelector('#start');
- 
-startButtonEl.addEventListener('click', startTimer);
+startButtonEl.addEventListener("click", startGame);
 
-
-
-
-
-const startTimer = () => {
-timeInterval = setInterval(function (){
-
-if(gameTimer > 1){
-    // Update screen with new time
-    timerEl.textContent = gameTimer + ' seconds remaining';
-    gameTimer--;
-
-}
- else if(gameTimer < 1 )
- {
-    timerEl.textContent = '';
-    clearInterval(timeInterval);
-    // Update Screen that the game is over and timer is cleared
+function startGame() {
+  abra.style.visibility = "visible";
+  kadabra.style.visibility = "visible";
+  timeInterval = setInterval(function () {
+    if (gameTimer >= 1) {
+      // Update screen with new time
+      timerEl.textContent = gameTimer + " seconds remaining";
+      gameTimer--;
+    } else if (gameTimer <= 0) {
+      clearInterval(timeInterval);
+      abra.style.visibility = "hidden";
+      kadabra.style.visibility = "hidden";
+      // Update Screen that the game is over and timer is cleared
+    }
+  }, 1000);
 }
 
-}, 1000);
+function nextQuestion() {}
 
+function userAnswerCorrect() {}
 
-}
-
+const questionList = [
+  {
+    question: "What is the meaning of life",
+    answers: [
+      (questionA = { text: "", correct: false }),
+      (questionB = { text: "", correct: false }),
+      (questionC = { text: "", correct: false }),
+      (questionD = { text: "", correct: false }),
+    ],
+  },
+  {
+    question: "What is the meaning of life",
+    answers: [
+      (questionA = { text: "", correct: false }),
+      (questionB = { text: "", correct: false }),
+      (questionC = { text: "", correct: false }),
+      (questionD = { text: "", correct: false }),
+    ],
+  },
+  {
+    question: "What is the meaning of life",
+    answers: [
+      (questionA = { text: "", correct: false }),
+      (questionB = { text: "", correct: false }),
+      (questionC = { text: "", correct: false }),
+      (questionD = { text: "", correct: false }),
+    ],
+  },
+  {
+    question: "What is the meaning of life",
+    answers: [
+      (questionA = { text: "", correct: false }),
+      (questionB = { text: "", correct: false }),
+      (questionC = { text: "", correct: false }),
+      (questionD = { text: "", correct: false }),
+    ],
+  },
+  {
+    question: "What is the meaning of life",
+    answers: [
+      (questionA = { text: "", correct: false }),
+      (questionB = { text: "", correct: false }),
+      (questionC = { text: "", correct: false }),
+      (questionD = { text: "", correct: false }),
+    ],
+  },
+  {
+    question: "What is the meaning of life",
+    answers: [
+      (questionA = { text: "", correct: false }),
+      (questionB = { text: "", correct: false }),
+      (questionC = { text: "", correct: false }),
+      (questionD = { text: "", correct: false }),
+    ],
+  },
+  {
+    question: "What is the meaning of life",
+    answers: [
+      (questionA = { text: "", correct: false }),
+      (questionB = { text: "", correct: false }),
+      (questionC = { text: "", correct: false }),
+      (questionD = { text: "", correct: false }),
+    ],
+  },
+  {
+    question: "What is the meaning of life",
+    answers: [
+      (questionA = { text: "", correct: false }),
+      (questionB = { text: "", correct: false }),
+      (questionC = { text: "", correct: false }),
+      (questionD = { text: "", correct: false }),
+    ],
+  },
+];
