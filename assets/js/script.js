99b950738f4495310215dbfae6d616f43b075947
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-01-13 01:52:31 -0600

    got most of it working

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,126 +12,179 @@
 
 //Final conditions is when the timer reaches 0 or all the questions are answers
 // Then - use local storage to store initials (string) of the player.
-var mainEl = document.getElementById("main");
 let timerEl = document.getElementById("countDown");
 
-const questionEl = document.getElementById("theQuestion");
 let shuffleQuestions, currentQuestion;
 
-
-var gameTimer = 5;
+var gameTimer = 30;
 var timeInterval;
 
 const startButtonEl = document.getElementById("btnStart");
 const endButtonEl = document.getElementById("btnQuit");
+const nextButton = document.getElementById("next");
+const answerButtonEl = document.getElementById("answer-buttons");
+const questionContainer = document.getElementById("qContainer");
+const questionEl = document.getElementById("theQuestion");
 //const aButtonEl = document.querySelector("");
-
-startButtonEl.addEventListener("click", startGame);
-const questionContainer = document.getElementById("qContainer")
 
 startButtonEl.classList.remove("hide");
 endButtonEl.classList.remove("hide");
 
-function startGame() {
-timerEl.classList.remove("hide");
-startButtonEl.classList.add("hide");
-questionContainer.classList.remove("hide");
+startButtonEl.addEventListener("click", startGame);
+// nextButton.addEventListener('click', );
+nextButton.addEventListener("click", () => {
+  currentQuestion++;
+  nextQuestion();
+});
 
-shuffleQuestions = questionList.sort(()=> Math.random() - .5)
-nextQuestion();
+function startGame() {
+  timerEl.classList.remove("hide");
+  startButtonEl.classList.add("hide");
+
   timeInterval = setInterval(function () {
     if (gameTimer >= 1) {
       // Update screen with new time
       timerEl.textContent = gameTimer + " seconds remaining";
       gameTimer--;
     } else if (gameTimer <= 0) {
-        timerEl.classList.add("hide");
+      timerEl.classList.add("hide");
       clearInterval(timeInterval);
-      
+
       gameTimer = 30;
       return null;
       // Update Screen that the game is over and timer is cleared
     }
   }, 1000);
+  shuffleQuestions = questionList.sort(() => Math.random() - 0.5);
+  currentQuestion = 0;
+  questionContainer.classList.remove("hide");
+  nextQuestion();
 }
 
 function nextQuestion() {
-    displayQuestion(shuffleQuestions[currentQuestion])
+  clearState();
+  displayQuestion(shuffleQuestions[currentQuestion]);
 }
 
-function userAnswerCorrect() {}
+function displayQuestion(question) {
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add(".answerButton");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", userAnswer);
+    answerButtonEl.appendChild(button);
+  });
+}
 
+function clearState() {
+  clearStatus(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonEl.firstChild) {
+    answerButtonEl.removeChild(answerButtonEl.firstChild);
+  }
+}
+
+function userAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatus(document.body, correct);
+  Array.from(answerButtonEl.children).forEach((button) => {
+    setStatus(button, button.dataset.correct);
+  });
+  if (shuffleQuestions.length > currentQuestion + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButtonEl.innerText = "Restart";
+    startButtonEl.classList.remove("hide");
+  }
+}
+function setStatus(element, correct) {
+  clearStatus(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+function clearStatus(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 const questionList = [
   {
-    question: "What is the meaning of life",
+    question: "Which one of the following is a non-magnetic metal",
     answers: [
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "Nickel", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Iron", correct: false },
+      { text: "Cobalt", correct: false },
     ],
   },
   {
-    question: "What is the meaning of life",
+    question: "Which one of the following is a non-magnetic metal",
     answers: [
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "Nickel", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Iron", correct: false },
+      { text: "Cobalt", correct: false },
     ],
   },
   {
-    question: "What is the meaning of life",
+    question: "Which one of the following is a non-magnetic metal",
     answers: [
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "Nickel", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Iron", correct: false },
+      { text: "Cobalt", correct: false },
     ],
   },
   {
-    question: "What is the meaning of life",
+    question: "Which one of the following is a non-magnetic metal",
     answers: [
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "Nickel", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Iron", correct: false },
+      { text: "Cobalt", correct: false },
     ],
   },
   {
-    question: "What is the meaning of life",
+    question: "Which one of the following is a non-magnetic metal",
     answers: [
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "Nickel", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Iron", correct: false },
+      { text: "Cobalt", correct: false },
     ],
   },
   {
-    question: "What is the meaning of life",
+    question: "Which one of the following is a non-magnetic metal",
     answers: [
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "Nickel", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Iron", correct: false },
+      { text: "Cobalt", correct: false },
     ],
   },
   {
-    question: "What is the meaning of life",
+    question: "Which one of the following is a non-magnetic metal",
     answers: [
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "Nickel", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Iron", correct: false },
+      { text: "Cobalt", correct: false },
     ],
   },
   {
-    question: "What is the meaning of life",
+    question: "Which one of the following is a non-magnetic metal",
     answers: [
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
-      { text: "", correct: false },
+      { text: "Nickel", correct: false },
+      { text: "Copper", correct: true },
+      { text: "Iron", correct: false },
+      { text: "Cobalt", correct: false },
     ],
   },
 ];
